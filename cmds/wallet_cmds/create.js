ad6fde2992585de95a3f85e2fa30933669b1e961
Jack Itleaks <itleaks@126.com>
2018-09-28 00:44:19 +0800

    Encrypt user's private key
    Signed-off-by: Jack Itleaks <itleaks@126.com>

---
+++ cmds/wallet_cmds/create.js
@@ -0,0 +1,14 @@
+/*
+* js4eos Powered by Eosfavor
+* Jack Itleaks @2018
+* itleaks@126.com
+*/
+
+const Js4Eos = require('../../lib')
+
+exports.command = 'create'
+exports.desc = 'create default wallet'
+exports.builder = {}
+exports.handler = function (argv) {
+    Js4Eos.create().then(ret => Js4Eos.printJson(ret));
+}
