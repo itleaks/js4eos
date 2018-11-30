a43bb0e777599528bb038f6dca4691b162f55a02
Itleaks <itleaks@126.com>
2018-11-30 15:14:42 +0800

    Add new command 'wallet stop'
    Signed-off-by: Jack Itleaks <itleaks@126.com>

---
+++ cmds/wallet_cmds/stop.js
@@ -0,0 +1,45 @@
+/*
+* js4eos Powered by Eosfavor
+* Jack Itleaks @2018
+* itleaks@126.com
+*/
+const fs = require('fs')
+
+exports.command = 'stop'
+exports.desc = 'stop wallet daemon'
+exports.builder = {}
+
+function handle() {
+  bgpidFile = require('path').join(require('os').homedir(), ".js4eos_bg.pid")
+  var pid = 0;
+  if (fs.existsSync(bgpidFile)) {
+    try {
+      pid = fs.readFileSync(bgpidFile).toString();
+    } catch(e) {
+        console.log(e)
+    }
+  }
+  if (!pid) {
+    console.log("Not found bg process")
+    return;
+  }
+
+  setTimeout(() => {
+    try {
+      var isWin = process.platform === "win32"
+      if (isWin) {
+        var exec = require('child_process').exec
+        exec("taskkill /f /pid " + pid)
+      } else {
+        process.kill(pid, 'SIGHUP');
+      }
+      console.log("stop success")
+    } catch(e) {
+      console.log(e)
+    }
+  }, 1000);
+}
+
+exports.handler = function (argv) {
+  handle()
+}
