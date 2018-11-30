b1eb227609873dab6ca6476af74f47f478fe21f9
Itleaks <itleaks@126.com>
2018-11-30 18:55:46 +0800

    Fix issue path is wrong in windows when deploy a app
    Signed-off-by: Jack Itleaks <itleaks@126.com>

--- cmds/dapp_cmds/test.js
+++ cmds/dapp_cmds/test.js
@@ -3,14 +3,19 @@
 * Jack Itleaks @2018
 * itleaks@126.com
 */
-var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
+var path = require('path')
 
 exports.command = 'test'
 exports.desc = 'test'
 exports.builder = {}
 exports.handler = function (argv) {
     try {
-        let tmp = spawn('./node_modules/.bin/mocha')
+        var currentDir = process.cwd()
+        var binFile = path.join(currentDir, "node_modules")
+        binFile = path.join(binFile, ".bin")
+        binFile = path.join(binFile, "mocha")
+        let tmp = exec(binFile)
         tmp.stdout.pipe(process.stdout);
         tmp.stderr.pipe(process.stderr);
     } catch(e) {
