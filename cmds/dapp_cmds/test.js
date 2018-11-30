050205e1e1a61c7eb5fa161b9b9a7cf62fcd8b76
Itleaks <itleaks@126.com>
2018-12-01 00:33:21 +0800

    Allow to test one file and one test case
    Signed-off-by: Jack Itleaks <itleaks@126.com>

--- cmds/dapp_cmds/test.js
+++ cmds/dapp_cmds/test.js
@@ -6,16 +6,28 @@
 var exec = require('child_process').exec;
 var path = require('path')
 
-exports.command = 'test'
+exports.command = 'test [file]'
 exports.desc = 'test'
-exports.builder = {}
+exports.builder = yargs => yargs
+    .options("g", {
+        alias:'grep',
+        desc:"grep pattern",
+        type:'string',
+    })
 exports.handler = function (argv) {
     try {
         var currentDir = process.cwd()
         var binFile = path.join(currentDir, "node_modules")
         binFile = path.join(binFile, ".bin")
         binFile = path.join(binFile, "mocha")
-        let tmp = exec(binFile)
+        var cmd = binFile
+        if (argv.file) {
+            cmd = cmd + " " + argv.file
+        }
+        if (argv.grep) {
+            cmd = binFile + " -g " + argv.grep
+        }
+        let tmp = exec(cmd)
         tmp.stdout.pipe(process.stdout);
         tmp.stderr.pipe(process.stderr);
     } catch(e) {
