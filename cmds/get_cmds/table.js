47cee5c06d13b3fc087aa9c8ec92a96a353d31da
Jack Itleaks <itleaks@126.com>
2018-12-12 08:11:45 +0800

    Add more description for command 'get table'
    Signed-off-by: Jack Itleaks <itleaks@126.com>

--- cmds/get_cmds/table.js
+++ cmds/get_cmds/table.js
@@ -12,27 +12,32 @@ exports.builder = yargs => yargs
 .options("l", {
     alias:"limit",
     desc:"The maximum number of rows to return",
-    type:'string',
+    type:'number',
+    default:10,
 })
 .options("L", {
     alias:"lower",
     desc:"JSON representation of lower bound value of key, defaults to first",
     type:'string',
+    default:'',
 })
 .options("U", {
     alias:"upper",
     desc:"JSON representation of upper bound value value of key, defaults to last",
     type:'string',
+    default:'',
 })
 .options("i", {
     alias:"index",
     desc:"Index number, 1 - primary (first), 2 - secondary index (in order defined by multi_index), 3 - third index, etc.",
-    type:'string',
+    type:'number',
+    default:1,
 })
 .options("k", {
     alias:"key_type",
     desc:"The key type of --index, primary only supports (i64), all others support (i64, i128, i256, float64, float128, ripemd160, sha256).",
     type:'string',
+    default:'',
 })
 exports.handler = function (argv) {
     Js4Eos.getTable(argv.contract, argv.scope, argv.table, argv.lower, argv.upper, argv.limit, argv.key_type, argv.index).then(ret => Js4Eos.printJson(ret));
